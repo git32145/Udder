c1c58766e3543b0f6d1e40edf35113b7ffa20ee7
Ismail <ismailnagdi@yahoo.com>
2019-06-23 16:43:35 +0100

    Add viewWillLayout

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -15,9 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
-}
-
-func viewwillappear() {
+    override func viewWillLayoutSubviews() {
+        <#code#>
+    }
     
 }
+
+
